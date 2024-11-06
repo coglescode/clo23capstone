8d4154a994cef7b22ef580397974b5e38b326ef2
Leonard Cogles Bonne <coglescode@gmail.com>
2024-11-06 11:41:35 +0100

    Removing ConnectionString

--- FSO.Client/Program.cs
+++ FSO.Client/Program.cs
@@ -8,14 +8,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<MembersApiService>();
-builder.Services.AddDbContext<ClientDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ClientContext")));
+
+
+builder.Configuration.AddJsonFile("secrets.json",
+        optional: true,
+        reloadOnChange: true);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    builder.Services.AddDbContext<ClientDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionStrings:DefaultConnection")));
+    
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
